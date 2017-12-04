0142132431e61c8cc51ce5fb7a61d4dbdfcf8bac
Javlon Usmanov <javlon@nyu.edu>
2017-12-03 22:01:43 -0500

    personal-summary component introduced(no good name for it)

---
+++ src/app/personal-summary/personal-summary.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PersonalSummaryComponent } from './personal-summary.component';
+
+describe('PersonalSummaryComponent', () => {
+  let component: PersonalSummaryComponent;
+  let fixture: ComponentFixture<PersonalSummaryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PersonalSummaryComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PersonalSummaryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/personal-summary/personal-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-summary',
+  templateUrl: './personal-summary.component.html',
+  styleUrls: ['./personal-summary.component.css']
+})
+export class PersonalSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
