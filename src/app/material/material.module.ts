e48696505271d34890bce36049820015d3464514
Javlon Usmanov <javlon@nyu.edu>
2017-12-03 21:59:49 -0500

    added Gridlist to MaterialModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,27 +1,30 @@
 import { NgModule } from '@angular/core';
 
 import {
-  MatButtonModule,
-  MatMenuModule,
-  MatToolbarModule,
-  MatIconModule,
-  MatCardModule
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule, MatGridList, MatGridListModule
 } from '@angular/material';
 
 @NgModule({
-  imports: [
-    MatButtonModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule
-  ],
-  exports: [
-    MatButtonModule,
-    MatMenuModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatCardModule
-  ]
+    imports: [
+        MatButtonModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatGridListModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatMenuModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatGridListModule
+    ]
 })
-export class MaterialModule {}
+export class MaterialModule {
+}
