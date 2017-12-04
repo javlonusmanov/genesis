5cc91d1b8e1f4b8d2b1a9a22a64610b725bc1f5a
Javlon Usmanov <javlon@nyu.edu>
2017-12-03 22:00:35 -0500

    code-refactored

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
 import { PersonalSummaryComponent } from './personal-summary/personal-summary.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
     declarations: [
         AppComponent,
-        PersonalSummaryComponent
+        PersonalSummaryComponent,
+        HeaderComponent,
+        FooterComponent
     ],
     imports: [
         BrowserModule,
