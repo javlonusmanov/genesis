63b23178827f721d21faa8e7e37c46e57b65ee40
Javlon Usmanov <javlon@nyu.edu>
2018-04-16 22:51:08 -0400

    footer modified to be more responsive

--- src/app/footer-content/footer-content.component.ts
+++ src/app/footer-content/footer-content.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material';
 
 @Component({
-  selector: 'app-footer-content',
-  templateUrl: './footer-content.component.html',
-  styleUrls: ['./footer-content.component.css']
+    selector: 'app-footer-content',
+    templateUrl: './footer-content.component.html',
+    styleUrls: ['./footer-content.component.css']
 })
 export class FooterContentComponent implements OnInit {
+    ngOnInit(): void {
+    }
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+    constructor(iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
+        iconRegistry.addSvgIcon('github', sanitizer.bypassSecurityTrustResourceUrl('assets/github.svg'));
+        iconRegistry.addSvgIcon('email', sanitizer.bypassSecurityTrustResourceUrl('assets/at.svg'));
+        iconRegistry.addSvgIcon('linkedIn', sanitizer.bypassSecurityTrustResourceUrl('assets/linkedin-logo.svg'));
+    }
 }
