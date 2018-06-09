8cb4e7e432585633a472d630b934ec7eee7c3421
Javlon Usmanov <javlon@nyu.edu>
2018-06-09 16:28:09 -0400

    header component modified to support onclick functionality to toggle menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
-    styleUrls: ['./header.component.css']
+    styleUrls: ['./header.component.css'],
+    host: {
+        '(document:click)': 'toggleFromOutside()',
+    },
 })
 export class HeaderComponent implements OnInit {
-    constructor() {}
+    private toggleMenu: boolean = false;
 
+    constructor(private eRef: ElementRef) {
+    }
 
     ngOnInit(): void {
+    }
+
+    toggle() {
+        this.toggleMenu = !this.toggleMenu;
+    }
+
+    toggleFromOutside() {
+        if(!this.eRef.nativeElement.contains(event.target)) {
+            if(this.toggleMenu) {
+                this.toggle()
+            }
+        }
     }
 }
