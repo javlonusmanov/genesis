bb6495a2395f87a6f26da54025c849b47eb3573b
Javlon Usmanov <javlon@nyu.edu>
2018-07-11 23:31:43 -0400

    modified project structure to match docker container setup

--- genesis-server/routes/api.js
+++ genesis-server/routes/api.js
@@ -5,7 +5,15 @@ const router = express.Router();
 
 /* GET api listing. */
 router.get('/', (req, res) => {
-    res.send('api works');
+    res.json({
+        message: "API works"
+    });
+});
+
+router.get('/message', (req, res) => {
+    res.json({
+        message: "post works"
+    });
 });
 
 module.exports = router;
--- genesis-server/server.js
+++ genesis-server/server.js
@@ -5,9 +5,23 @@ const http = require('http');
 const bodyParser = require('body-parser');
 
 // Get our API routes
-const api = require('./server/routes/api');
+const api = require('./routes/api');
 
 const app = express();
+
+app.use((req, res, next) => {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization, Content-Type');
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    // Pass to next layer of middleware
+    next();
+});
 
 // Parsers for POST data
 app.use(bodyParser.json());
