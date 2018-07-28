c9f7efccfe3d9357b678ea1dcb3c53b64e9712c5
Javlon Usmanov <javlon@nyu.edu>
2018-07-28 01:14:24 -0400

    sorted and removed bloat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,31 @@
-import { AboutPageComponent } from './about-page/about-page.component';
-import { AngularFireModule } from 'angularfire2';
-import { AppComponent } from './app.component';
-import { BlogPageComponent } from './blog-page/blog-page.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { BrowserModule } from '@angular/platform-browser';
-import { FooterComponent } from './footer/footer.component';
-import { FooterContentComponent, PizzaPartyComponent } from './footer-content/footer-content.component';
-import { FormsModule } from '@angular/forms'
-import { HeaderComponent } from './header/header.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { HttpClientModule } from '@angular/common/http';
-import { MaterialModule } from './material/material.module';
-import { NgModule } from '@angular/core';
-import { PersonalSummaryComponent } from './personal-summary/personal-summary.component';
-import { RouterModule, Routes } from '@angular/router';
-import { WorkPageComponent } from './work-page/work-page.component';
-import { environment } from '../environments/environment';
-import { AngularFireStorageModule } from 'angularfire2/storage';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AboutPageComponent } from "./about-page/about-page.component";
+import { AngularFireAuthModule } from "angularfire2/auth";
+import { AngularFireDatabaseModule } from "angularfire2/database";
+import { AngularFireModule } from "angularfire2";
+import { AngularFireStorageModule } from "angularfire2/storage";
+import { AngularFirestoreModule } from "angularfire2/firestore";
+import { AppComponent } from "./app.component";
+import { BlogPageComponent } from "./blog-page/blog-page.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BrowserModule } from "@angular/platform-browser";
+import { environment } from "../environments/environment";
+import { FooterComponent } from "./footer/footer.component";
+import { FooterContentComponent, PizzaPartyComponent } from "./footer-content/footer-content.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HeaderComponent } from "./header/header.component";
+import { HomePageComponent } from "./home-page/home-page.component";
+import { HttpClientModule } from "@angular/common/http";
+import { MaterialModule } from "./material/material.module";
+import { NgModule } from "@angular/core";
+import { PersonalSummaryComponent } from "./personal-summary/personal-summary.component";
+import { RouterModule, Routes } from "@angular/router";
+import { WorkPageComponent } from "./work-page/work-page.component";
 
 const routes: Routes = [
-    { path: '', component: HomePageComponent },
-    { path: 'work', component: WorkPageComponent },
-    { path: 'blog', component: BlogPageComponent },
-    { path: 'about', component: AboutPageComponent }
+    { path: "", component: HomePageComponent },
+    { path: "work", component: WorkPageComponent },
+    { path: "blog", component: BlogPageComponent },
+    { path: "about", component: AboutPageComponent }
 ];
 
 @NgModule({
@@ -44,15 +45,17 @@ const routes: Routes = [
         PizzaPartyComponent
     ],
     imports: [
+        AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
+        AngularFireDatabaseModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFireStorageModule, // imports firebase/storage only needed for storage features
+        AngularFirestoreModule, // imports firebase/firestore, only needed for database features
         BrowserAnimationsModule,
         BrowserModule,
-        AngularFireModule.initializeApp(environment.firebase, 'genesis-app'),
-        AngularFirestoreModule, // imports firebase/firestore, only needed for database features
-        AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
-        AngularFireStorageModule, // imports firebase/storage only needed for storage features
         FormsModule,
         HttpClientModule,
         MaterialModule,
+        ReactiveFormsModule,
         RouterModule.forRoot(routes)
     ],
     providers: [],
