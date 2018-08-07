d2dbf90bef67810b1aee5d8822084872123f2e03
Javlon Usmanov <javlon@nyu.edu>
2018-08-06 22:19:45 -0400

    added logic to listen on scroll events

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,8 @@
 import { Component, ElementRef, EventEmitter, Output } from "@angular/core";
 import { HeaderService } from "./header.service";
+import { WindowService } from "../utility-services/window.service";
+import { fromEvent } from "rxjs";
+// import 'rxjs/add/operator/bufferCount';
 
 @Component({
     selector: "app-header",
@@ -7,8 +10,15 @@ import { HeaderService } from "./header.service";
     styleUrls: ["./header.component.css"]
 })
 export class HeaderComponent {
+    public scrolled: boolean = false;
 
-    constructor(private headerService: HeaderService) {}
+    constructor(private headerService: HeaderService, private windowService: WindowService) {
+        fromEvent(windowService.get(), "scroll")
+        .subscribe(() => {
+            windowService.get().pageYOffset > 50 ? this.scrolled = true : this.scrolled = false;
+        });
+        console.log(windowService.get().scroll)
+    }
 
     openSideNav() {
         this.headerService.change();
