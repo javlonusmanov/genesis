443bf7264b0949a75d3e87e5894ded402829c635
Javlon Usmanov <javlon@nyu.edu>
2020-04-04 02:08:42 -0400

    ng update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,22 @@
 import { AboutPageComponent } from "./about-page/about-page.component";
-import { AngularFireAuthModule } from "@angular/fire/auth";
-import { AngularFireDatabaseModule } from "@angular/fire/database";
-import { AngularFireModule } from "@angular/fire";
-import { AngularFireStorageModule } from "@angular/fire/storage";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { AppComponent } from "./app.component";
 import { BlogPageComponent } from "./blog-page/blog-page.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { BrowserModule } from "@angular/platform-browser";
-import { environment } from "../environments/environment";
 import { FooterComponent } from "./footer/footer.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HeaderComponent } from "./header/header.component";
 import { HomePageComponent } from "./home-page/home-page.component";
 import { HttpClientModule } from "@angular/common/http";
 import { MaterialModule } from "./material/material.module";
-import { NgModule } from "@angular/core";
+import { NgModule } from "@angular/core"
 import { PersonalSummaryComponent } from "./personal-summary/personal-summary.component";
-import { RouterModule, Routes } from "@angular/router";
 import { WorkPageComponent } from "./work-page/work-page.component";
 import { WindowService } from "./utility-services/window.service";
 import { ResumeComponent } from './resume/resume.component';
 import { ServicesComponent } from './services/services.component';
 import { TestimonialComponent } from './testimonial/testimonial.component';
 import { PizzaPartyComponent } from './footer/footer.component';
-
-const routes: Routes = [
-    { path: "", component: HomePageComponent },
-    { path: "work", component: WorkPageComponent },
-    { path: "blog", component: BlogPageComponent },
-    { path: "about", component: AboutPageComponent }
-];
 
 @NgModule({
     declarations: [
@@ -54,7 +40,6 @@ const routes: Routes = [
         HttpClientModule,
         MaterialModule,
         ReactiveFormsModule,
-        RouterModule.forRoot(routes)
     ],
     providers: [
         WindowService
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { AbstractControl, FormBuilder, FormGroup, NgForm, Validators } from "@angular/forms";
 import { MatSnackBar } from "@angular/material";
-import { AngularFirestore, AngularFirestoreCollection } from "@angular/fire/firestore";
 import { Observable } from "rxjs/Observable";
 
 export class Message {
@@ -17,45 +16,6 @@ export class Message {
     styleUrls: ["./footer.component.scss"]
 })
 export class FooterComponent implements OnInit {
-    @ViewChild("f") myNgForm;
-
-
-    messageForm: FormGroup;
-    private messages: Observable<Message[]>;
-    private messagesCollection: AngularFirestoreCollection<Message>;
-
-    ngOnInit(): void {
-    }
-
-    constructor(private formBuilder: FormBuilder,
-                private db: AngularFirestore,
-                private snackBar: MatSnackBar) {
-
-        this.messagesCollection = db.collection<Message>("messages");
-        this.messages = this.messagesCollection.valueChanges();
-
-        this.messageForm = this.formBuilder.group({
-            name: [null, Validators.required],
-            email: [null, [Validators.required, Validators.email]],
-            message: [null, Validators.required]
-        });
-    }
-
-    onSend() {
-        this.messagesCollection.add({
-            name: this.messageForm.controls["name"].value,
-            email: this.messageForm.controls["email"].value,
-            message: this.messageForm.controls["message"].value,
-            created_date: new Date()
-        } as Message);
-
-        this.myNgForm.resetForm();
-        this.messageForm.markAsUntouched();
-
-        this.snackBar.openFromComponent(PizzaPartyComponent, {
-            duration: 2000
-        });
-    }
 }
 
 
